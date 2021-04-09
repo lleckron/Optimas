1c2d8ff6708e3c5be88cde7dd20d09795b11454f
lpleckron <lpleckron@bsu,edu>
2021-04-08 23:19:32 -0400

    create InputRomanNumeral, user enters up to 10 char roman numeral & checks String be under 3000 limit

---
+++ src/main/java/InputRomanNumeral.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class InputRomanNumeral {
+
+    public InputRomanNumeral(){
+
+    }
+    public String enterRomanNumeral() {
+        String romanNumeral;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            romanNumeral = scanner.nextLine();
+            if (!romanNumeralBelow3000(romanNumeral)) {
+                romanNumeral = "";
+            }
+        } while(romanNumeral.isEmpty());
+        return romanNumeral;
+    }
+
+    public Boolean romanNumeralBelow3000(String romanNumeral) {
+        System.out.println("Enter up to 10 roman numerals. Their value will be calculated.");
+        if ((romanNumeral.length() > 3) && (romanNumeral.substring(0,3).equals("MMM"))) {
+            System.err.println("The roman numeral you have entered is over 3000. Please enter another number.");
+            return false;
+        } else if((romanNumeral.length() > 6) && (romanNumeral.substring(0,6).equals("DDDDDD"))) {
+            System.err.println("The roman numeral you have entered is over 3000. Please enter another number.");
+            return false;
+        } else if (romanNumeral.length() > 10) {
+            System.err.println("You have entered too many roman numerals. Please enter another number.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
