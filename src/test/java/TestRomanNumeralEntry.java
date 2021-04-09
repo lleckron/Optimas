1359c8d852b9aba533eebdafc36a87a5f322a4eb
lpleckron <lpleckron@bsu,edu>
2021-04-09 01:00:18 -0400

    reformat tests to pass

--- src/test/java/TestRomanNumeralEntry.java
+++ src/test/java/TestRomanNumeralEntry.java
@@ -6,7 +6,7 @@ public class TestRomanNumeralEntry {
     @Test
     public void TestRomanNumeralBelow3000() {
         InputRomanNumeral inputRomanNumeral = new InputRomanNumeral();
-        String romanNumeral = "DDDDDDI";
+        String romanNumeral = "MDXIDXIDD";
         boolean testBoolean = inputRomanNumeral.romanNumeralBelow3000(romanNumeral);
         Assertions.assertFalse(testBoolean);
     }
