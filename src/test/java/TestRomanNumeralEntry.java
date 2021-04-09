a83a0db13737ac704e5839c59f08dcaeb8337fb0
lpleckron <lpleckron@bsu,edu>
2021-04-09 02:34:20 -0400

    add TestInvalidCharacterInput to ensure user does not input invalid characters

--- src/test/java/TestRomanNumeralEntry.java
+++ src/test/java/TestRomanNumeralEntry.java
@@ -2,10 +2,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestRomanNumeralEntry {
-
+    InputRomanNumeral inputRomanNumeral = new InputRomanNumeral();
+    RomanNumeralCalculator romanNumeralCalculator = new RomanNumeralCalculator();
     @Test
     public void TestRomanNumeralBelow3000() {
-        InputRomanNumeral inputRomanNumeral = new InputRomanNumeral();
         String romanNumeral = "MDXIDXIDD";
         boolean testBoolean = inputRomanNumeral.romanNumeralBelow3000(romanNumeral);
         Assertions.assertFalse(testBoolean);
@@ -13,18 +13,22 @@ public class TestRomanNumeralEntry {
 
     @Test
     public void TestRomanNumeralStringBelowTenChars() {
-        InputRomanNumeral inputRomanNumeral = new InputRomanNumeral();
         String romanNumeral = "IIIIIIIIIII";
         boolean testBoolean = inputRomanNumeral.romanNumeralBelow3000(romanNumeral);
         Assertions.assertFalse(testBoolean);
     }
 
     @Test
+    public void TestInvalidCharacterInput() {
+        String romanNumeral = null;
+        boolean testBoolean = inputRomanNumeral.romanNumeralBelow3000(romanNumeral);
+        Assertions.assertFalse(testBoolean);
+    }
+
+    @Test
     public void TestRomanNumeralCalculator() {
-        RomanNumeralCalculator romanNumeralCalculator = new RomanNumeralCalculator();
         String romanNumeral = "XLVII";
         int total = romanNumeralCalculator.calculateRomanNumeralString(romanNumeral);
         Assertions.assertEquals(67,total);
     }
-
 }
